bd4cc7e2a966cc391f29be1289dc8f8c5d32f862
jhan-dev <jhan.technology@gmail.com>
2023-03-23 14:54:36 -0700

    Connect mongoose to mongoDB

--- app.js
+++ app.js
@@ -9,8 +9,9 @@ app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(express.static("publicPath"));
+app.use(express.static("public"));
 
+mongoose.connect("mongodb://localhost:27017/wikiDB", {userNewUrlParser: true});
 
 app.listen(3000, function(){
   console.log("Server started on port 3000");
