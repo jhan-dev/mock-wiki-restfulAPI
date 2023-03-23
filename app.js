e78c1bb5ee293ef3157f6abbf858d5ba77d53e88
jhan-dev <jhan.technology@gmail.com>
2023-03-23 15:01:05 -0700

    Fixed minor mongod bug

--- app.js
+++ app.js
@@ -11,7 +11,14 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost:27017/wikiDB", {userNewUrlParser: true});
+mongoose.connect("mongodb://localhost:27017/wikiDB");
+
+const articleSchema = {
+  title: String,
+  content: String
+};
+
+const Article = mongoose.model("Article", articleSchema);
 
 app.listen(3000, function(){
   console.log("Server started on port 3000");
