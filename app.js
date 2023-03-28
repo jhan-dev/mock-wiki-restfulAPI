05a23ecd8df2987f90d40cb15e493d8de73e01cd
jhan-dev <jhan.technology@gmail.com>
2023-03-28 15:15:43 -0700

    Refactored code to chain requests

--- app.js
+++ app.js
@@ -70,40 +70,6 @@ app.route("/articles/:articleTitle")
   })
 });
 
-
-
-// app.get("/articles", function(req, res){
-  //   Article.find({}).then(function(foundArticles){
-//     console.log(foundArticles);
-//     res.send(foundArticles);
-//   })
-//   .catch(function(err){
-//     console.log(err)
-//     res.send(err)
-//   })
-// });
-
-// app.post("/articles", function(req, res){
-//   console.log(req.body.title)
-//   console.log(req.body.content)
-
-//   const newArticle = new Article({
-//     title: req.body.title,
-//     content: req.body.content
-//   });
-
-//   newArticle.save();
-// });
-
-// app.delete("/articles", function(req, res){
-//   Article.deleteMany({}).then(function(){
-//     res.send("Successfully deleted all articles.")
-//   })
-//   .catch(function(err){
-//     res.send(err)
-//   })
-// });
-
 app.listen(3000, function(){
   console.log("Server started on port 3000");
 });
